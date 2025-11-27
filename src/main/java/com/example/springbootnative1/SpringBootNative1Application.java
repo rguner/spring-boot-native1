7f6c4bb9859e0440f3f784f4b1031c2772312260
Ramazan Güner <ramazan.guner@atmosware.turkcell.com.tr>
2025-11-27 15:11:32 +0300

    first commit

---
+++ src/main/java/com/example/springbootnative1/SpringBootNative1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootnative1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNative1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNative1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootnative1/SpringBootNative1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootnative1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNative1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
